d2cbffda510fb9ff01a30c425afa31794e5ff984
palepinkday <palepinkday@gmail.com>
2022-09-15 01:14:20 +0900

    0915

--- javascript/assets/js/common.js
+++ javascript/assets/js/common.js
@@ -6,30 +6,38 @@ const modalClose = document.querySelector(".modal__close");
 const modalCont = document.querySelector(".modal__cont");
 
 modalBtn.addEventListener("click", () => {
-    modalCont.classList.add("show");
-    modalCont.classList.remove("hide");
+  modalCont.classList.add("show");
+  modalCont.classList.remove("hide");
 });
 modalClose.addEventListener("click", () => {
-    modalCont.classList.add("hide");
+  modalCont.classList.add("hide");
 });
 
-// 탭 메뉴
+//탭 메뉴
 const tabBtn = document.querySelectorAll(".modal__box .tabs > div");
 const tabCont = document.querySelectorAll(".modal__box .cont > div");
 
+//클릭하면 active붙여주기
 tabBtn.forEach((element, index) => {
-    element.addEventListener("click", (event) => {
-        event.preventDefault();
+  //버튼 클릭하면
+  element.addEventListener("click", (event) => {
+    //클릭시 event는 창 맨 위로 올라가는 것
+    event.preventDefault(); //event는 클릭하면 위로 올라가는 것을 막아주기 위함
 
-        tabBtn.forEach(li => {
-            li.classList.remove("active");
-        })
-        element.classList.add("active");
-
-        tabCont.forEach(div => {
-            div.style.display = "none";
-        });
-
-        tabCont[index].style.display = "block";
+    //클래스 active를 모두 제거함
+    //3개를 선택해서 엑티브 없애줘야함
+    tabBtn.forEach((el) => {
+      //el을 li로 적어줘도 됨 목록안에 있는 이름들(alphabet 등등)이니깐
+      el.classList.remove("active"); //class지워주는 것 : classList.remove
     });
+    //내가 클릭한 버튼에 active를 추가함
+    element.classList.add("active");
+
+    //버튼을 클릭하면 모든 자식 박스 숨김
+    tabCont.forEach((div) => {
+      div.style.display = "none";
+    });
+    //[i]클릭한 버튼과 [i]관련 박스를 보이게 함
+    tabCont[index].style.display = "block";
+  });
 });
