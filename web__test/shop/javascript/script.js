e5bd81b9e3f35592dfec58c974045352cfe72076
palepinkday <palepinkday@gmail.com>
2022-11-11 21:48:32 +0900

    1111

--- web__test/shop/javascript/script.js
+++ web__test/shop/javascript/script.js
@@ -1,32 +1,44 @@
 jQuery(document).ready(function () {
-  $(".navi>li").mouseover(function () {
+  $(".navi>li")
+    .mouseover(function () {
       $(".submenu").stop().slideDown(500);
-  }).mouseout(function () {
+    })
+    .mouseout(function () {
       $(".submenu").stop().slideUp(500);
+    });
+  // 좌우슬라이드
+  //   setInterval(function () {
+  //     $(".slidelist").delay(1000);
+  //     $(".slidelist").animate({ marginLeft: -1200 });
+  //     $(".slidelist").delay(2000);
+  //     $(".slidelist").animate({ marginLeft: -2400 });
+  //     $(".slidelist").delay(2000);
+  //     $(".slidelist").animate({ marginLeft: 0 });
+  //     $(".slidelist").delay(2000);
+  //   });
+
+  // 상하슬라이드
+  setInterval(() => {
+    $(".slidelist").delay(1000);
+    $(".slidelist").animate({ marginTop: -300 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginTop: -600 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginTop: 0 });
+    $(".slidelist").delay(2000);
   });
-    
-    setInterval(function () { 
-        $(".slidelist").delay(1000);
-        $(".slidelist").animate({ marginLeft: -1200 });
-        $(".slidelist").delay(2000);
-        $(".slidelist").animate({ marginLeft: -2400 });
-        $(".slidelist").delay(2000);
-        $(".slidelist").animate({ marginLeft: 0 });
-        $(".slidelist").delay(2000);
 
-    });
+  // $(function () {
+  $(".tabmenu>li>a").click(function () {
+    $(this).parent().addClass("active").siblings().removeClass("active");
+    return false;
+  });
+  // });
 
-    $(function () {
-        $(".tabmenu>li>a").click(function () {
-            $(this).parent().addClass("active").siblings().removeClass("active");
-            return false;
-        }); 
-    });
-    
-    $(".notice>ul>li").click(() => {
-        $(".modal").addClass("active");
-    });
-      $(".btn").click(() => {
-        $(".modal").removeClass("active");
-    });
+  $(".notice>ul>li:first").click(() => {
+    $(".modal").addClass("active");
+  });
+  $(".btn").click(() => {
+    $(".modal").removeClass("active");
+  });
 });
